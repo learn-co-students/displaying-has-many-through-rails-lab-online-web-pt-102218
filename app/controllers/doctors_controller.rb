81ca1ace6c6df0ad3208819dff14e1c4e976cabd
github username <github email address>
2019-04-07 16:03:45 -0500

    Done.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,14 @@
 class DoctorsController < ApplicationController
+
+    def new
+        @doctor = Doctor.new
+    end
+
+    def show
+        @doctor = Doctor.find_by(params[:id])
+    end
+
+    def index
+        @doctors = Doctor.all
+    end
 end
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,14 @@
 class PatientsController < ApplicationController
+
+    def new
+        @patient = Patient.new
+    end
+
+    def show
+        @patient = Patient.find_by(params[:id])
+    end
+
+    def index
+        @patients = Patient.all
+    end
 end
